4f75fa367237f2d2a3a9f255b86664061f473243
a.aguri <>
2018-09-30 16:11:10 +0900

    マスの設定

---
+++ src/Board.js
@@ -0,0 +1,28 @@
+import React, { Component } from 'react';
+import Square from './Square';
+
+class Board extends Component {
+  renderSquare(i) {
+    return <Square key={i} value={this.props.squares[i]} onClick={() => this.props.onClick(i)} />;
+  }
+
+
+  createBorad(){
+    let board = [];
+    for(let i=0;i<5;i++){
+      board.push(
+        <div className="board-row" key={"b"+i}>{
+        [0,1,2,3,4].map(j=>{
+          return this.renderSquare(i*5 + j);
+        })}
+      </div>);
+    }
+    return board;
+  }
+
+  render() {
+    return (this.createBorad());
+  }
+}
+
+export default Board;
--- src/index.js
+++ src/index.js
@@ -1,8 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import Game from './Game';
 import './index.css';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+ReactDOM.render(
+  <Game />,
+  document.getElementById('root')
+);
